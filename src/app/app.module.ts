744a14e6d5e2283d3116c2d2b593f6b83fa5d909
kbeinert <bei15001@byui.edu>
2019-07-11 14:14:35 -0600

    Beginning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MovieComponent } from './movie/movie.component';
+import { MovieListComponent } from './movie/movie-list/movie-list.component';
+import { MovieEditComponent } from './movie/movie-list/movie-edit/movie-edit.component';
+import { MovieItemComponent } from './movie/movie-list/movie-item/movie-item.component';
+import { MovieDetailComponent } from './movie/movie-detail/movie-detail.component';
+import { MovieViewComponent } from './movie/movie-view/movie-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MovieComponent,
+    MovieListComponent,
+    MovieEditComponent,
+    MovieItemComponent,
+    MovieDetailComponent,
+    MovieViewComponent
   ],
   imports: [
     BrowserModule,
